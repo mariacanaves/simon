24c2872355cf2b4713dfcc280220525fb52af532
mariacanaves <mariacanavea@alumnat.iesdamiahuguet.net>
2024-02-23 08:39:16 +0100

    Conseguim 1 color

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
     Mostra la notificació al panell notification
     Mostra el color hexadecimal passat per paràmetre
  */
+let colors=["#b2e2f2","#c5c6c8","#d8bfd8","#ffda9e","#ffbf80","#b0f2c2","#aaff80","#96ffe0", "#ff9cf0","#d1c2ff","#d2bead","#a2c8cc","#d98cb3","#d6ff80","#ff6961","#84b6f4"]
+let llista=[]
+let i=0
+
+function iniciarPartida(){
+    llista.push(colors[Math.trunc(Math.random()*colors.length)])
+    console.log(llista)
+    showNotification(llista[llista.length-1])
+
+}
 
 function showNotification(message) {
     console.log(message)
@@ -12,4 +22,15 @@ function showNotification(message) {
     setTimeout(function() {
         notification.style.display = 'none';
     }, 2000);
+}
+
+function mirarcolor(color){
+console.log(color + "-" + llista[0])
+
+    if(color==llista[i]){
+        window.alert('gg')
+    }
+    else{
+        window.alert('Has perdut :c')
+    }
 }
