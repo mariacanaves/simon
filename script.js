a229a3404059768ee6bee6dc64c9f14809dd25c3
mariacanaves <mariacanavea@alumnat.iesdamiahuguet.net>
2024-02-28 13:38:37 +0100

    Acabat?

--- script.js
+++ script.js
@@ -4,13 +4,13 @@
  */
 let colors=["#b2e2f2","#c5c6c8","#d8bfd8","#ffda9e","#ffbf80","#b0f2c2","#aaff80","#96ffe0", "#ff9cf0","#d1c2ff","#d2bead","#a2c8cc","#d98cb3","#d6ff80","#ff6961","#84b6f4"]
 let llista=[]
-let i=0
+let posiciollista=0
+let nivell=0
 
 function iniciarPartida(){
     llista.push(colors[Math.trunc(Math.random()*colors.length)])
     console.log(llista)
     showNotification(llista[llista.length-1])
-
 }
 
 function showNotification(message) {
@@ -25,12 +25,28 @@ function showNotification(message) {
 }
 
 function mirarcolor(color){
-console.log(color + "-" + llista[0])
+console.log(color + "-" + llista[posiciollista])
 
-    if(color==llista[i]){
-        window.alert('gg')
+    if(color==llista[posiciollista]){
+        if(posiciollista==llista.length-1){
+            llista.push(colors[Math.trunc(Math.random()*colors.length)])
+            posiciollista=0
+            showNotification(llista[llista.length-1])
+            nivell++
+            document.getElementById('intents').innerHTML='Nivell: ' +nivell
+
+        }
+        else{
+            posiciollista++
+        }
     }
     else{
         window.alert('Has perdut :c')
+        nivell=0
+        document.getElementById('intents').innerHTML='Nivell: ' +nivell
+        llista=[]
+        posiciollista=0
+        document.getElementById('reintentar').innerHTML= "Torna a jugar"
+
     }
 }
