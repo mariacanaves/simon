e3eadcca14fb64c660929ea495cdefb15dc6d5f5
mariacanaves <mariacanavea@alumnat.iesdamiahuguet.net>
2024-03-05 12:59:02 +0100

    Slider i colors més diferenciats

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
-/*
-    Mostra la notificació al panell notification
-    Mostra el color hexadecimal passat per paràmetre
- */
-let colors=["#b2e2f2","#c5c6c8","#d8bfd8","#ffda9e","#ffbf80","#b0f2c2","#aaff80","#96ffe0", "#ff9cf0","#d1c2ff","#d2bead","#a2c8cc","#d98cb3","#d6ff80","#ff6961","#84b6f4"]
+// Variables i arrays
+let colors=["#f5f5dc","#fdfd96","#d8bfd8","#e00000","#ffbf80","#836953","#366959","#96ffe0", "#2b064d","#d1c2ff","#d2bead","#a2c8cc","#d98cb3","#d6ff80","#ff6961","#84b6f4"]
 let llista=[]
 let posiciollista=0
 let nivell=0
+let temps=2000
+// Config slider
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    temps=this.value
+    console.log('Temps: '+temps)
+}
 
+// Inici de la partida(crear el color
 function iniciarPartida(){
     llista.push(colors[Math.trunc(Math.random()*colors.length)])
     console.log(llista)
     showNotification(llista[llista.length-1])
 }
-
+// Enviar el color
 function showNotification(message) {
     console.log(message)
     var notification = document.getElementById('notification');
     notification.style.backgroundColor=message
     notification.style.display = 'block';
+    console.log('Temps2: '+temps)
 
+    // temps del color
     setTimeout(function() {
         notification.style.display = 'none';
-    }, 2000);
+    }, document.getElementById("myRange").value);
 }
 
 function mirarcolor(color){
 console.log(color + "-" + llista[posiciollista])
-
+    // mira si el color esta be
     if(color==llista[posiciollista]){
         if(posiciollista==llista.length-1){
             llista.push(colors[Math.trunc(Math.random()*colors.length)])
@@ -40,6 +51,7 @@ console.log(color + "-" + llista[posiciollista])
             posiciollista++
         }
     }
+    // si perds t'ho diu
     else{
         window.alert('Has perdut :c')
         nivell=0
